4ab47e486086a5d1d4ec5e79d5e9697c5b7929e0
Mikko Pitkänen <moukku.santeri@gmail.com>
2019-10-31 18:04:35 +0200

    Modified some formatting

--- src/Strategy/src/Multiply.java
+++ src/Strategy/src/Multiply.java
@@ -3,8 +3,7 @@ public class Multiply implements Calculate {
 
 	@Override
 	public int count(int i, int j) {
-		// TODO Auto-generated method stub
-		return i*j;
+		return i * j;
 	}
 
 }
--- src/Strategy/src/StrategyTester.java
+++ src/Strategy/src/StrategyTester.java
@@ -4,23 +4,21 @@ import java.util.List;
 public class StrategyTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		List<Calculate> operations = new ArrayList<>();
 
 		operations.add(new Sum());
 		operations.add(new Multiply());
 		operations.add(new Divide());
 
-		
 		int i = 6;
 		int j = 2;
 
-		//Java8 syntax
-		operations.stream().forEach((cal)->System.out.println(cal.count(i, j)));
-		
+		// Java8 syntax
+		operations.stream().forEach((cal) -> System.out.println(cal.count(i, j)));
+
 		System.out.println();
-		//prior Java8
-		for(Calculate cal : operations) {
+		// prior Java8
+		for (Calculate cal : operations) {
 			System.out.println(cal.count(i, j));
 		}
 
